7a77d5c9ef84c6e452ee1f18ed025fb74a7a8b3e
matsuim <mcm710@gmail.com>
2017-12-19 23:19:54 -0500

    classes per semester tab done

--- shinyApp_BDegreeAdvisor1.R
+++ shinyApp_BDegreeAdvisor1.R
@@ -75,7 +75,18 @@ ui <- navbarPage("BDegreeAdvisor", theme = shinytheme("united"),
                    )
                  )
                  ),
-                 tabPanel("Classes available per semester"),
+                 tabPanel("Classes available per semester",
+                          sidebarLayout(
+                            sidebarPanel(
+                              selectInput("conc_name", "Concentration", choices = names(conc_list), selected = conc_list[29]),
+                              selectInput("term", "Semester", choices = c("fall", "winter", "spring"), selected = "fall")
+                            ),
+                            mainPanel(
+                              h4("Courses", style= {"font-family:Georgia;color:#ad1d28"}),
+                              dataTableOutput("course_table")
+                            )
+                          )
+                          ),
                  tabPanel("Find Your Next Job",
                           sidebarLayout(
                             sidebarPanel(
@@ -497,6 +508,65 @@ server <- function(input, output) {
     } else {stop('One of the concentrations does not have a table presented')}
   }, escape = FALSE)  
 
+  
+  ################################################# Tab 3   ################################################# 
+  
+  courses <- reactive({
+    # read in json and store info to variable
+    url <- "https://cab.brown.edu/asoc-api/?output=json&page=asoc.rjs&route=search&term=999999"
+    all_courses <- jsonlite::fromJSON(url, flatten=TRUE)[["courses"]]
+    all_courses <- all_courses[, c(1:3, 8)]
+    
+    # get required courses for concentration & check that concentration is valid
+    tryCatch(suppressMessages(required <- conc_req(input$conc_name)), error=function(e) return(stop("Concentration not found.")))
+    
+    # pull out course codes
+    required <- unlist(stringr::str_match_all(required[, 1:2],"[A-Z]{3,4}[:space:][0-9]{4}[A-Z]*"))
+    required <- as.character(required)
+    
+    # split up course code into department, course code, and letter
+    required <- data.frame(code = required, 
+                           subj = stringr::str_extract(required, "^[A-Z]{3,4}"),
+                           course_code = as.numeric(stringr::str_extract(required, "[0-9]{4}")), 
+                           letter = stringr::str_extract(required, "[A-Z]*$"),
+                           stringsAsFactors = FALSE)
+    
+    # make columns for up to three possible terms listed that the course runs in 
+    all_courses <- dplyr::mutate(all_courses, term1 = as.numeric(sapply(all_courses$terms, function(x) x[1])), 
+                                 term2 = as.numeric(sapply(all_courses$terms, function(x) x[2])), 
+                                 term3 = as.numeric(sapply(all_courses$terms, function(x) x[3])))
+    # remove unneeded column
+    all_courses <- all_courses[,-4]
+    
+    # set semester 
+    if(input$term == "fall"){
+      sem <- 201710
+    } else if (input$term == "spring") {
+      sem <- 201720
+    } else if (input$term == "winter") {
+      sem <- 201715
+    }
+    
+    # filter courses by selected semester
+    current_term <- dplyr::filter(all_courses, term1 == sem | term2 == sem | term3 == sem)  
+    # split course code into subject, course code, and letter and remove any course lists
+    current_term <- dplyr::filter(current_term, !stringr::str_detect(current_term$code, 'XLIST'))
+    current_term <- dplyr::mutate(current_term, course_code = stringr::str_extract(current_term$code, "[0-9]{4}"))
+    current_term <- dplyr::mutate(current_term, letter = stringr::str_extract(current_term$code, "[A-Z]*$"))
+    # change type of course code from chr to num
+    current_term$course_code <- as.numeric(current_term$course_code)
+    
+    #filter for rows in current term courses that are in the respective fields of required courses
+    final <- dplyr::filter(current_term, subj %in% required$subj &
+                             course_code %in% required$course_code &
+                             # subset only course code and title
+                             letter %in% required$letter)[,1:2] 
+    final <- as.data.frame(final)
+    names(final) <- c("Course Code", "Course Title")
+    final
+  })
+  output$course_table <- renderDataTable({courses()})
+  
   
   ################################################# Tab 4   ################################################# 
  
